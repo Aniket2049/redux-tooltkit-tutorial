c6c549bdf12e1bcd0b3c480a2bcc968b0fe332e7
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-03-23 20:25:31 +0530

    Revert "add create asyncthunk options"
    This reverts commit b04dff076920fb2623fa0a203be22ee4bda9cb9e.

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,13 +1,5 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import cartItems from "../../cartItems";
-
-const url = "https://course-api.com/react-useReducer-cart-project";
-
-export const getCartItems = createAsyncThunk("cart/getCartItems", () => {
-  return fetch(url)
-    .then((resp) => resp.json())
-    .catch((err) => console.log(err));
-});
 
 const cartSlice = createSlice({
   name: "cart",
@@ -45,20 +37,6 @@ const cartSlice = createSlice({
       });
       state.amount = amount;
       state.total = total;
-    },
-  },
-  extraReducers: {
-    [getCartItems.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [getCartItems.fulfilled]: (state, action) => {
-      console.log(action);
-
-      state.isLoading = false;
-      state.cartItems = action.payload;
-    },
-    [getCartItems.rejected]: (state) => {
-      state.isLoading = false;
     },
   },
 });
