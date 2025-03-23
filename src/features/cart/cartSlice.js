166142b006fd73fc1eadf189f633b963ab25e737
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-03-23 17:30:14 +0530

    add slice

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    cartItems: [],
+    amount: 0,
+    total: 0,
+    isLoading: true,
+  },
+});
+
+export default cartSlice.reducer;
