2f9940b3d3a80c8a560da30e6c84c0328906cb0c
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-03-23 19:35:45 +0530

    add toggle amount functionality

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -13,9 +13,24 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    removeItem: (state, action) => {
+      console.log(action);
+      const itemID = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemID);
+    },
+    increase: (state, { payload }) => {
+      console.log(payload);
+
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount++;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount--;
+    },
   },
 });
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
